387a29cbfa9e1cd3260ed30cc181238276989ec3
Rudhra Bharathy <bharathyganeshan@gmail.com>
2024-02-05 01:23:55 +0530

    completed footer and some bug fixes

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
   const body = document.querySelector("body");
 
   $(window).scroll(function () {
-    
     $(".navbar").toggleClass("color-change", $(document).scrollTop() > 20);
     $(".tansprent_bg").toggleClass("size-change", $(document).scrollTop() > 20);
+    $("body").toggleClass("bg-color-change", $(document).scrollTop() > 8000);
   });
 
   const inputOpen = document.querySelector(".search-btn");
@@ -47,13 +47,56 @@ toggleButton.click(function () {
   vimeoPlayer.getPaused().then(function (paused) {
     if (paused) {
       vimeoPlayer.play();
-      $('.video-triangle').css('display', 'none');
-      toggleButton.addClass('videoPauseCursor');
-      toggleButton.removeClass('videoPlayCursor');
+      $(".video-triangle").css("display", "none");
+      toggleButton.addClass("videoPauseCursor");
+      toggleButton.removeClass("videoPlayCursor");
     } else {
       vimeoPlayer.pause();
-      toggleButton.removeClass('videoPauseCursor');
-      toggleButton.addClass('videoPlayCursor');
+      toggleButton.removeClass("videoPauseCursor");
+      toggleButton.addClass("videoPlayCursor");
     }
   });
-}); 
+});
+
+$(".fa-arrow-up").hover(function () {
+  $(this).toggleClass("fa-bounce");
+});
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+$(".fa-arrow-up").on("click", scrollToTop);
+
+document.getElementById(
+  "footerlogo"
+).style.transform = `scaleY(1) translateY(0px)`;
+
+$(window).scroll(function () {
+  var scrollPosition = window.scrollY || window.pageYOffset;
+  if (scrollPosition >= 10666) {
+    var newSP = scrollPosition - 10666;
+    var scaley = Math.min(1 + newSP * 0.0058, 2.5);
+    var newScaleY = scaley.toFixed(3);
+    var translate = Math.min(newSP * 0.197, 85);
+    var newTranslate = translate.toFixed(0);
+    if (scaley === 2.5 && translate === 85) {
+      newScaleY = 1;
+      newTranslate = 433;
+    }
+    document.getElementById("footerlogo").style.transition =
+      "transform 0.3s ease-in-out"; // Add transition
+    document.getElementById(
+      "footerlogo"
+    ).style.transform = `scaleY(${newScaleY}) translateY(${parseInt(
+      newTranslate
+    )}px)`;
+  } else if (scrollPosition <= 10666) {
+    document.getElementById(
+      "footerlogo"
+    ).style.transform = `scaleY(1) translateY(0px)`;
+  }
+});
